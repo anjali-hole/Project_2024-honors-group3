35b00e385ef4e7122679b38c0cce468ad4ed373c
anjali-hole <121920668+anjali-hole@users.noreply.github.com>
2024-10-10 13:09:20 -0500

    generate sorted and generate reverse sorted function complete

--- Code/data_generation.cpp
+++ Code/data_generation.cpp
@@ -3,7 +3,13 @@
 #include "mpi.h"
 
 void generate_sorted(int* local_data, size_t local_data_size, int comm_size, int rank) {
-
+    // Calculate the global start index for this rank
+    size_t global_start = rank * local_data_size;
+    
+    // Generate sorted data for this rank
+    for (size_t i = 0; i < local_data_size; ++i) {
+        local_data[i] = global_start + i;
+    }
 }
 
 void generate_sorted_1percent_perturbed(int* local_data, size_t local_data_size, int comm_size, int rank) {
@@ -15,7 +21,13 @@ void generate_random(int* local_data, size_t local_data_size, int comm_size, int
 }
 
 void generate_reverse_sorted(int* local_data, size_t local_data_size, int comm_size, int rank) {
-
+    // Calculate the global end index for this rank
+    size_t global_end = (comm_size - rank) * local_data_size;
+    
+    // Generate reverse sorted data for this rank
+    for (size_t i = 0; i < local_data_size; ++i) {
+        local_data[i] = global_end - i - 1;
+    }
 }
 
 bool check_data_sorted(int* local_data, size_t local_data_size, int comm_size, int rank) {
